9ebf7a1500b025c7952302bf03741f015867a63f
dym0080 <308960474@qq.com>
2019-10-16 11:57:13 +0800

    add router feature

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -9,7 +9,6 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
-
   constructor(
     private messageService: MessageService
   ) { }
@@ -17,5 +16,10 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
